4c70e08381cbac43eabc416b7e85e0baf90ea205
cezar <cezar_pm@hotmail.com>
2020-06-22 16:51:46 -0300

    custom-express config

---
+++ src/configs/custom-express.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = require('../routes');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(router);
+
+module.exports = app;
