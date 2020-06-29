8619c5ed05f65ab15f621b0edd15b7f9fec13147
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2020-06-29 19:45:57 -0300

    Criando o rate limit

--- src/configs/custom-express.js
+++ src/configs/custom-express.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 import router from '../routes';
 
 const app = express();
 
 app.use(cors());
+app.use(rateLimit({
+  windowMs: 5 * 60 * 1000, max: 200, // 5min, 200 requests
+}));
 app.use(router);
 
 export default app;
