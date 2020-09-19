1f96d51ccfc1b906d8556709ad7ddedc93736618
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2020-09-19 14:20:37 -0300

    varios: Configuração do TypeORM

--- src/configs/custom-express.js
+++ src/configs/custom-express.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
+import { createConnection } from 'typeorm';
 
 import router from '../routes';
+import { databaseConfig } from './server-config';
 
 const app = express();
 
 app.use(cors());
+
 app.use(rateLimit({
   windowMs: 5 * 60 * 1000, max: 200, // 5min, 200 requests
 }));
+
 app.use(router);
+
+// Start TypeORM
+(async () => {
+  await createConnection(databaseConfig);
+})();
 
 export default app;
--- src/controllers/status.controller.js
+++ src/controllers/status.controller.js
@@ -1,8 +1,20 @@
+import { getManager } from 'typeorm';
+import assert from 'assert';
 import { commit, enviroment } from '../configs/server-config';
 
 const StatusController = {
   async getStatus(req, res) {
-    res.send({ commit, enviroment });
+    let dbOnline = false;
+    try {
+      const queryResult = await getManager().query('SELECT 1+1 as queryResult');
+      assert(queryResult[0].queryResult === '2');
+      dbOnline = true;
+    } catch (error) {
+      dbOnline = false;
+      console.error(error);
+    }
+
+    res.send({ commit, enviroment, dbOnline });
   },
 };
 
