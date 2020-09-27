bd12777c91e9e2db0a5a99f7b4ca2f3460c17e6a
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2020-09-27 02:46:45 -0300

    match & summoner: Pequeno refactor para pipe

--- src/controllers/match.controller.js
+++ src/controllers/match.controller.js
@@ -3,13 +3,11 @@ import { getMatchBymatchID, getMatchHistoryByName } from '../services/riotMatch.
 const MatchController = {
   async getMatchHistoryByName(req, res) {
     const { name } = req.params;
-    const matchHistory = await getMatchHistoryByName(name);
-    res.send(matchHistory);
+    return await getMatchHistoryByName(name) |> res.send;
   },
   async getMatchBymatchID(req, res) {
     const { matchId } = req.params;
-    const match = await getMatchBymatchID(matchId);
-    res.send(match);
+    return await getMatchBymatchID(matchId) |> res.send;
   },
 };
 
--- src/controllers/summoner.controller.js
+++ src/controllers/summoner.controller.js
@@ -3,8 +3,7 @@ import { getSummonnerByName } from '../services/riotSummoner.service';
 const SummonerController = {
   async getSummonnerByName(req, res) {
     const { name } = req.params;
-    const invocador = await getSummonnerByName(name);
-    res.send(invocador);
+    return await getSummonnerByName(name) |> res.send;
   },
 };
 
