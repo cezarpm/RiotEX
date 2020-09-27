b2fdb3cf14a7339745024a9d80c90a1a12490c73
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2020-09-27 01:44:12 -0300

    general.test.js: Criado arquivo de testes para o _common

---
+++ test/general.test.js
@@ -0,0 +1,10 @@
+import { generateRandomHash } from '../src/_common/general';
+
+describe('Testes das funções de utils do arquivo _common/general', () => {
+  it('Função generateRandomHash retorna hash de 64 chars ', async () => {
+    const hash = await generateRandomHash(parseInt(Math.random() * 1000, 10));
+
+    expect(hash).toBeDefined();
+    expect(hash).toHaveLength(64);
+  });
+});
