5be95e62d01aab679991707cdfbabc8bb7ca6370
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2020-09-27 03:08:11 -0300

    varios: Removidos testes

--- test/axios-config.test.js
+++
@@ -1,8 +0,0 @@
-describe('Testes da configuração do axios', () => {
-  it('Import do axiosInstance retorna instancia do axios configurada', async () => {
-    const axios = (await import('../src/configs/axios-config')).default;
-
-    expect(axios.get).toBeDefined();
-    expect(axios.post).toBeDefined();
-  });
-});
