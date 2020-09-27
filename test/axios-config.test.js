210366d948e23a7d09319bf5815eb05b57f8131e
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2020-09-27 02:56:59 -0300

    varios: Removidos testes e2e por incompatibilidade com pipe operator

--- test/axios-config.test.js
+++ test/axios-config.test.js
@@ -1,11 +1,13 @@
+import env from '../env';
+
 describe('Testes da configuração do axios', () => {
   it('Import do axiosInstance retorna instancia do axios configurada', async () => {
     const axios = (await import('../src/configs/axios-config')).default;
 
     expect(axios.get).toBeDefined();
     expect(axios.post).toBeDefined();
-    expect(axios.defaults.baseURL).toEqual(process.env.BASE_URL);
-    expect(axios.defaults.headers['X-Riot-Token']).toEqual(process.env.API_KEY);
+    expect(axios.defaults.baseURL).toEqual(env.BASE_URL);
+    expect(axios.defaults.headers['X-Riot-Token']).toEqual(env.API_KEY);
     expect(axios.defaults.headers['X-Riot-Token']).not.toBe('undefined');
   });
 });
